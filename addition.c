f7d6f4c4c5fcb6093c71ea7b790ed77fbbff2741
ishacodes-20 <ishagbadave20@gmail.com>
2026-08-10 09:08:01 +0000

    addition

---
+++ addition.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c, sum;
+
+    a = 10;
+    b = 20;
+    c = 30;
+    sum = a + b + c;
+
+    printf("First number: %d\n", a);
+    printf("Second number: %d\n", b);
+    printf("Third number: %d\n", c);
+    printf("Sum of three numbers: %d\n", sum);
+
+    return 0;
+}
