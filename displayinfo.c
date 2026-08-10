499f7d902b8a54ffb308625ebbc473e7595f0af6
ishacodes-20 <ishagbadave20@gmail.com>
2026-08-10 09:04:17 +0000

    display info

---
+++ displayinfo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) 
+{
+    printf("Name:Isha Girish Badave\n");
+    printf("Roll No:70\n");
+    printf("Class: fy btech CSE\n");
+    printf("Division: C\n");
+    return 0;
+}
